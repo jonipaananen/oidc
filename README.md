48c02dc7c654529cd79995d4f74365d86568b95c
jonipaananen <joni@paananen.dev>
2026-04-03 02:37:50 +0200

    made a POC for Maconomy auth

---
+++ README.md
@@ -0,0 +1,63 @@
+# Simple POC for Maconomy API OIDC authentication
+
+This python script authenticates the user with Entra OIDC, exchanges the code from entra to a Maconomy reconnect token,
+which then can be used for authenticated calls.
+
+## Authentication flow
+
+1. The authorization url is built, given the Entra tenant id and client id:
+    `https://login.microsoftonline.com/TENANT_ID/oauth2/authorize?client_id=CLIENT_ID&scope=openid&response_type=code&redirect_uri=REDIRECT_URI`
+2. Ff the authentication is successful, entra redirects back to `http://localhost/`, with the authorization code.
+3. The script has started an http server, and extracts the code. The code can then be used to call Maconomy and
+    exchange it for a reconnect token:
+    
+    ```python
+   encoded_code = base64.b64encode(f"<http://localhost/>:{code}".encode())
+
+    url = "https://b3iaccess.deltekenterprise.com/maconomy-api/auth/b3"
+
+    headers = {
+        "Authorization": f"X-OIDC-Code {encoded_code.decode("utf-8")}",
+        "maconomy-authentication": "X-Reconnect",
+    }
+    response = requests.get(url, headers=headers)
+    reconnect_token = response.headers['Maconomy-Reconnect']
+   ```
+4. Given the reconnect token, it's now possible to make authenticated requests:
+
+    ```python
+    url = "https://b3iaccess.deltekenterprise.com/maconomy-api/environment/b3?variables=user.employeeinfo.name1"
+    headers = {
+        "authorization": f"X-Reconnect {reconnect_token}",
+        "maconomy-authentication": f"X-Reconnect",
+    }
+    response = requests.get(url, headers=headers)
+   ```
+   Keep in mind that the reconnect token might have been updated in the last response, so for future requests, save it
+   from the `Maconomy-Reconnect` header
+
+## How to run
+
+Set up a .env file:
+```commandline
+cp template.env .env
+```
+
+Enter the client and tenant in the [.env](.env) file
+
+Set up a virtual environment:
+```commandline
+python -m venv .
+```
+Powershell: ``.venv/Scripts/Activate.ps1``
+
+bash: ``.venv/bin/activate``
+
+```commandline
+pip install -r requirements.txt
+```
+
+execute:
+```commandline
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,105 @@
+import base64
+import os
+import urllib.parse
+import webbrowser
+import http.server
+import threading
+
+import requests
+from dotenv import load_dotenv
+import traceback
+import urllib
+from urllib.parse import urlparse
+
+
+
+load_dotenv()
+
+authority_url = os.environ["AUTHORITY_URL"]
+token_endpoint = f"{authority_url}/oauth2/v2.0/token"
+authorization_endpoint = f"{authority_url}/oauth2/v2.0/authorize"
+client_id = os.environ["CLIENT_ID"]
+scope = os.environ["SCOPE"]
+callback_url = os.environ["REDIRECT_URL"]
+
+
+def init_auth_code_flow():
+
+    params = {
+        "client_id": client_id,
+        "response_type": "code",
+        "scope": scope,
+        "redirect_uri": callback_url,
+    }
+    authorization_url = f"{authorization_endpoint}?{urllib.parse.urlencode(params)}"
+    webbrowser.open(authorization_url)
+
+def set_reconnect_header(code):
+
+    encoded_code = base64.b64encode(f"<http://localhost/>:{code}".encode())
+
+    url = "https://b3iaccess.deltekenterprise.com/maconomy-api/auth/b3"
+
+    headers = {
+        "Authorization": f"X-OIDC-Code {encoded_code.decode("utf-8")}",
+        "maconomy-authentication": "X-Reconnect",
+    }
+    response = requests.get(url, headers=headers)
+
+    global reconnect_token
+    reconnect_token = response.headers['Maconomy-Reconnect']
+
+
+class AuthCodeResponseHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            set_reconnect_header(self.extract_code())
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(f"Reconnect token is: {reconnect_token}".encode())
+        except Exception as e:
+            raise e
+        finally:
+            threading.Thread(target=self.server.shutdown, daemon=True).start()
+
+    def response_error(self):
+        self.send_response(500)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(traceback.format_exc().encode())
+
+    def response_success(self, tokens: str):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(tokens.encode())
+
+    def extract_code(self) -> str:
+        host = self.headers.get("Host")
+        full_url = f"http://{host}{self.path}"
+        parsed_url = urlparse(full_url)
+        return urllib.parse.parse_qs(parsed_url.query).get("code")[0]
+
+    # Silence logging
+    def log_message(self, format, *args):
+        return
+
+
+def get_user_info():
+    url = "https://b3iaccess.deltekenterprise.com/maconomy-api/environment/b3?variables=user.employeeinfo.name1"
+    headers = {
+        "authorization": f"X-Reconnect {reconnect_token}",
+        "maconomy-authentication": f"X-Reconnect",
+    }
+    response = requests.get(url, headers=headers)
+    print(response.json())
+
+
+if __name__ == '__main__':
+    init_auth_code_flow()
+    server = http.server.HTTPServer(("0.0.0.0", 80), AuthCodeResponseHandler)
+    server.serve_forever()
+    get_user_info()
+
